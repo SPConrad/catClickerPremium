0ed2601299fb33cb40864233f3037e5c706e49c9
Konrad9 <konrad9@gmail.com>
2016-08-08 20:01:15 -0400

    Cat Clicker Pro Premium
    Some refactoring of original Premium code thanks to udacity's example.
    
    Implemented Pro feature of admin mode

--- main.js
+++ main.js
@@ -1,87 +1,216 @@
-$(function(){
+
 	var model = {
-		init: function() {
-			if (!localStorage.catArray){
-				localStorage.catArray = JSON.stringify([]);
-			} 
-			localStorage.catArray = JSON.stringify([]);
-		},
-		add: function(obj){
-			var data = JSON.parse(localStorage.catArray);
-            data.push(obj);
-            localStorage.catArray = JSON.stringify(data);
-		},
-		getCats: function(){
-			return JSON.parse(localStorage.catArray);
-		},
-		incrementClick: function(num){
-			var catArray = JSON.parse(localStorage.catArray)
-			catArray[num].clickCount += 1;
-			localStorage.catArray = JSON.stringify(catArray);
-		},
-		getCat: function(num){
-			this.incrementClick(num);
-			var returnString = JSON.parse(localStorage.catArray)[num];
-			return {'name': returnString.name, 'clickCount': returnString.clickCount, 'url': returnString.url};
-		}
-	};
-
-
-	var octopus = {
-		addCats: function(numOfCats) {
-			for (var i = 0; i < 5; i++){
-				model.add({"name": i, "clickCount": 0,"url": "img/cat" + i + ".jpg"})
+		adminMode: false, 
+		currentCat: null,
+		cats: 
+		[
+			{
+				clickCount : 0,
+				name: 'Jeff',
+				imgSrc : 'img/jeff.jpg',
+				imgAttribution: 'WhereIsItFromt'
+			},
+			{
+				clickCount : 0,
+				name: 'Brad',
+				imgSrc : 'img/brad.jpg',
+				imgAttribution: 'WhereIsItFromt'
+			},
+			{
+				clickCount : 0,
+				name: 'Ryan',
+				imgSrc : 'img/ryan.jpg',
+				imgAttribution: 'WhereIsItFromt'
+			},
+			{
+				clickCount : 0,
+				name: 'Vinny',
+				imgSrc : 'img/vinny.jpg',
+				imgAttribution: 'WhereIsItFromt'
+			},
+			{
+				clickCount : 0,
+				name: 'Drew',
+				imgSrc : 'img/drew.jpg',
+				imgAttribution: 'WhereIsItFromt'
+			},
+			{
+				clickCount : 0,
+				name: 'Austin',
+				imgSrc : 'img/austin.jpg',
+				imgAttribution: 'WhereIsItFromt'
 			}
-		},
-		getCats: function(){
-			return model.getCats();
-		},
-		getCat: function(num) {
-			imageView.render(model.getCat(num));
-		},
-		init: function() {
-			model.init();	
-			var startingCats = 5;
-			this.addCats(startingCats);
-			listView.init();
-			imageView.init();		
-		}
+		]
 	};
 
-	var listView = {
-		init: function(){
-			this.catList = $('#buttons');
-			var newButton = $('#catButton');
-			listView.render();
-			$(":button").each(function(i, elem) {
-				$(elem).click(function(e){
-					octopus.getCat(i);
-				})
-			})
-		},
-		render: function(){
-			var htmlStr = '';
-			octopus.getCats().forEach(function(cat){
-				htmlStr += '<li><button type="button" id="catButton' + cat.name 
-				+ '">Cat ' + cat.name + '</button></li>';
-			});
-			this.catList.html(htmlStr);
-		}
-	};
 
-	var imageView = {
-		init: function(){
-			this.displayArea = $('#bigCat').get(0);
-			this.clickCounter = $('#clickCounter').get(0);
-			var url = 'img/noCat.jpg';
-			imageView.render({'name': 'Starter', 'url': url, 'clickCount': 0});
-		},
-		render: function(cat){
-			var htmlStr = '<img id="bigCat" class="bigCatImage" src="' + cat.url + '">';
-			this.displayArea.innerHTML  = htmlStr;
-			this.clickCounter.innerHTML = cat.clickCount;
-		}
-	};
+/* ======= Octopus ======= */
 
-	octopus.init();
-});
+var octopus = {
+
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
+
+        // tell our views to initialize
+        adminView.init();
+        catListView.init();
+        catView.init();
+    },
+
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+    incrementCounter: function() {
+        model.currentCat.clickCount++;
+        catView.render();
+        adminView.render();
+    },
+
+    ////open up admin options:
+    changeAdminMode: function() {
+    	model.adminMode = !model.adminMode;
+    	adminView.render();
+    },
+    checkAndChange: function(oldValue, newValue){
+    	if (oldValue != newValue){
+    		return newValue;
+    	} else {
+    		return oldValue;
+    	}
+    },
+
+    commitAdminChanges: function() {
+    	console.log("commit");
+    	var inputtedName = adminView.catName.value;
+    	var inputtedClickCount = adminView.catClickCount.value;
+    	var inputtedURL = adminView.catURL.value;
+
+    	model.currentCat.name = this.checkAndChange(model.currentCat.name, inputtedName);
+    	model.currentCat.clickCount = this.checkAndChange(model.currentCat.clickCount, inputtedClickCount);
+    	model.currentCat.imgSrc = this.checkAndChange(model.currentCat.imgSrc, inputtedURL);
+
+    }
+};
+
+
+/* ======= View ======= */
+
+var adminView = {
+	init: function() {
+		this.adminArea = document.getElementById('adminArea');	
+		this.adminForm = document.getElementById('adminForm');	
+		this.catName = document.getElementById('admin-cat-name');
+		this.catClickCount = document.getElementById('admin-cat-clickCount');
+		this.catURL = document.getElementById('admin-cat-URL');
+		this.confirmButton = document.getElementById('admin-confirm-button');
+		this.cancelButton = document.getElementById('admin-cancel-button');
+		this.adminButton = document.getElementById('adminButton');
+		this.adminButton.addEventListener('click', function(){
+			octopus.changeAdminMode();
+		});
+		
+		this.confirmButton.addEventListener('click', function() {
+			octopus.commitAdminChanges();
+		});
+
+		this.cancelButton.addEventListener('click', function() {
+			octopus.changeAdminMode();
+		})
+		adminView.render();
+	},
+	render: function() {
+		if (model.adminMode == true){
+			this.adminForm.style.visibility = "visible";
+			cat = octopus.getCurrentCat();
+			this.catName.value = cat.name;
+			this.catClickCount.value = cat.clickCount;
+			this.catURL.value = cat.imgSrc;
+		} else if (model.adminMode == false) {
+			this.adminForm.style.visibility = "hidden";
+		}
+	}
+}
+
+var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // on click, increment the current cat's counter
+        this.catImageElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+    render: function() {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgSrc;
+    }
+};
+
+var catListView = {
+
+    init: function() {
+        // store the DOM element for easy access later
+        this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        var cat, elem, i;
+        // get the cats we'll be rendering from the octopus
+        var cats = octopus.getCats();
+
+        // empty the cat list
+        this.catListElem.innerHTML = '';
+
+        // loop over the cats
+        for (i = 0; i < cats.length; i++) {
+            // this is the cat we're currently looping over
+            cat = cats[i];
+
+            // make a new cat list item and set its text
+            elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
+            elem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                };
+            })(cat));
+
+            // finally, add the element to the list
+            this.catListElem.appendChild(elem);
+        }
+    }
+};
+
+// make it go!
+octopus.init();
